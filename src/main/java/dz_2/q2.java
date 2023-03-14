45bbce64b15b04ba7b50b2da18aa45ee87e1c32b
shpakau <shpakau@yahoo.com>
2023-03-14 18:09:15 +0300

    home_work q2

--- src/main/java/dz_2/q2.java
+++ src/main/java/dz_2/q2.java
@@ -1,4 +1,25 @@
 package dz_2;
 
-public class q2 {
-}
+import java.io.FileWriter;
+import java.io.IOException;
+
+    public class q2 {
+        public static void main(String[] args) {
+            q2test();
+        }
+        public static void q2test() {
+            String str="TEST";
+            StringBuilder strResult=new StringBuilder();
+            for (int i = 0; i < 99; i++) {
+                strResult.append(str+" ");
+            }
+
+            try (FileWriter writer=new FileWriter("TEST.txt")){
+                writer.write(String.valueOf(strResult));
+            }
+            catch (IOException e){
+                System.out.println("Ошибка");
+            }
+
+        }
+    }

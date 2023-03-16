d1423f922aafd94a22eb8977de0fe6cf2bf80afe
shpakau <shpakau@yahoo.com>
2023-03-16 18:41:59 +0300

    home_work dz_3 itog

--- src/main/java/dz_3/q3.java
+++ src/main/java/dz_3/q3.java
@@ -1,4 +1,33 @@
 package dz_3;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class q3 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("животные");
+        list.add("4");
+        list.add("зубр");
+        list.add("кабан");
+        list.add("кролик");
+        list.add("12");
+        list.add("1");
+
+        System.out.println("Первоначальный список: " + list);
+
+        Iterator<String> iterator = list.iterator();
+        while(iterator.hasNext()){
+            String element = iterator.next();
+            try{
+                Integer.parseInt(element);
+                iterator.remove();
+            } catch (NumberFormatException e){
+
+            }
+        }
+
+        System.out.println("Итоговый список: " + list);
+
+    }
 }
